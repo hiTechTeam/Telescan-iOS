e2d783d7382203a4c98d94fd13a67f438655212b
RuslanChukavin <r66cha@gmail.com>
2025-11-06 06:14:18 +0300

    Initial Commit

---
+++ TelescaniOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TelescaniOS
+//
+//  Created by Ruslan Chukavin on 06.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TelescaniOS/TelescaniOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelescaniOSApp.swift
+//  TelescaniOS
+//
+//  Created by Ruslan Chukavin on 06.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct TelescaniOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
